33b34d94eacc984c37def9bee6b9558944f4064d
PatriotRossii <patriotrossii2019@mail.ru>
2021-11-07 14:20:33 +0500

    Add stage 2. Daisy to Bob

---
+++ Case 202. The case of Bob and Daisy/2. Daisy to Bob/sketch.js
@@ -0,0 +1,187 @@
+/*
+
+Officer: 9405617
+CaseNum: 202-1-28461770-9405617
+
+Case 202 - The case of Bob and Daisy - stage 2
+
+Here’s another letter kid. This time it’s from Daisy (aka. Woz).
+Decode it to uncover more about Woz and Job’s dastardly plan.
+
+Discover the hidden code by commenting out all text commands except
+those which produce pink filled text with red outline.
+Only comment out text commands - leave fill & stroke commands uncommented.
+
+There are many possible ways of investigating this case, but you
+should use ONLY the following commands:
+
+  // comments are all that are needed for this case.
+  Do NOT add any new lines of code.
+
+*/
+
+var letterFont;
+
+function preload()
+{
+	letterFont = loadFont('Melissa.otf');
+}
+
+function setup()
+{
+	createCanvas(697,513);
+	textFont(letterFont);
+	textSize(33);
+}
+
+function draw()
+{
+	background(255);
+
+	fill(255,255,0);
+	stroke(0,0,255);
+	//text("kissed", 54,132);
+	//text("up", 502,132);
+	//text("How", 8,99);
+	fill(255,165,0);
+	//text("can", 113,165);
+	//text("will", 538,240);
+	fill(255,192,203);
+	//text("you", 185,99);
+	fill(255,255,0);
+	//text("How", 381,99);
+	fill(255,165,0);
+	//text("banking", 354,165);
+	fill(255,192,203);
+	//text("sky,", 9,165);
+	stroke(0,255,0);
+	//text("and", 57,165);
+	//text("return.", 563,165);
+	fill(0,255,255);
+	stroke(0,0,255);
+	//text("side,", 318,204);
+	//text("having", 122,99);
+	//text("be", 611,240);
+	stroke(255,0,0);
+	//text("united", 12,281);
+	//text("you", 217,204);
+	//text("longing", 396,240);
+	fill(255,255,0);
+	//text("I", 53,99);
+	//text("think", 151,165);
+	stroke(0,0,0);
+	//text("my", 331,132);
+	fill(0,255,255);
+	stroke(0,255,0);
+	//text("I", 139,132);
+	stroke(0,0,0);
+	//text("have", 214,240);
+	//text("this", 368,204);
+	//text("many", 426,99);
+	fill(255,165,0);
+	//text("small", 412,204);
+	//text("I", 424,132);
+	//text("night", 596,132);
+	stroke(0,0,255);
+	//text("at", 255,204);
+	//text("x", 67,413);
+	stroke(255,0,255);
+	//text("longer", 105,240);
+	//text("long", 157,132);
+	stroke(0,0,255);
+	//text("it", 579,99);
+	//text("spring,", 68,204);
+	stroke(0,255,0);
+	//text("your", 454,165);
+	//text("feels", 518,204);
+	stroke(255,0,0);
+	//text("My", 8,33);
+	//text("I'm", 317,165);
+	//text("the", 30,204);
+	//text("I", 95,165);
+	//text("darling", 45,33);
+	//text("Forever", 8,347);
+	//text("the", 558,132);
+	fill(0,255,255);
+	stroke(0,255,0);
+	//text("we", 654,99);
+	//text("you", 272,132);
+	//text("Da", 8,413);
+	stroke(255,0,0);
+	//text("miss", 71,99);
+	stroke(0,255,0);
+	//text("?", 125,281);
+	//text("to", 263,240);
+	//text("Bob,", 111,33);
+	fill(255,192,203);
+	//text("this", 352,240);
+	fill(255,255,0);
+	//text("last", 9,132);
+	fill(255,165,0);
+	//text("arms.", 365,132);
+	//text("?", 463,240);
+	fill(0,255,255);
+	stroke(0,0,0);
+	//text("of", 246,165);
+	fill(255,192,203);
+	stroke(255,0,255);
+	//text("desolate.", 572,204);
+	fill(255,255,0);
+	//text("around", 223,99);
+	fill(255,165,0);
+	stroke(255,0,0);
+	//text("I", 196,240);
+	fill(0,255,255);
+	stroke(255,0,255);
+	//text("stare", 442,132);
+	fill(255,255,0);
+	stroke(0,0,0);
+	//text("How", 9,240);
+	stroke(255,0,0);
+	//text("only", 203,165);
+	//text("hold", 229,132);
+	fill(255,192,203);
+	stroke(0,0,0);
+	//text("do", 167,240);
+	//text("again", 71,281);
+	fill(255,255,0);
+	stroke(255,0,255);
+	//text("?", 121,132);
+	//text("since", 601,99);
+	fill(255,165,0);
+	stroke(0,255,0);
+	//text("isy", 32,413);
+	fill(255,192,203);
+	stroke(0,0,255);
+	//text("my", 284,204);
+	//text("much", 54,240);
+	fill(255,255,0);
+	stroke(0,255,0);
+	//text("you.", 275,165);
+	stroke(255,0,255);
+	//text("When", 481,240);
+	//text("on", 426,165);
+	stroke(255,0,0);
+	//text("Even", 630,165);
+	fill(0,255,255);
+	//text("to", 200,132);
+	fill(255,192,203);
+	stroke(0,0,255);
+	//text("months", 480,99);
+	stroke(255,0,255);
+	//text("in", 310,132);
+	stroke(255,0,0);
+	text("town", 466,204);
+	stroke(0,0,0);
+	//text("we", 576,240);
+	//text("in", 9,204);
+	//text("yours,", 85,347);
+	//text("without", 138,204);
+	stroke(255,0,0);
+	text("place", 327,99);
+	text("is", 555,99);
+	text("the", 289,99);
+	text("swift", 501,165);
+	text("store", 292,240);
+	text("at", 529,132);
+}
